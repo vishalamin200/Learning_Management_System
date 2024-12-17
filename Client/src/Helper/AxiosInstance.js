19d9a46ffa01e6c81286119da50238a1247b7559
Vishal Amin <vishalamin200@gmail.com>
2024-12-17 19:34:42 +0530

    Update the fevicon and change server address

--- Client/src/Helper/AxiosInstance.js
+++ Client/src/Helper/AxiosInstance.js
@@ -3,9 +3,7 @@ import axios from "axios";
 
 const AxiosInstance = axios.create({
 
-
-    // baseURL: `https://192.168.1.101:4050/user`,
-    baseURL: "https://localhost:4050/user",     //server default url
+    baseURL: `https://codeacademy.root.sx:4050/user`,
 
     withCredentials: true,
     timeout: 12000,
