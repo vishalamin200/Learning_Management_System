3db40877d1c2aaeef06eebd398333f6734c141d1
Vishal Amin <vishalamin200@gmail.com>
2026-02-21 01:21:54 +0530

    updated navigation in editCourse

--- Client/src/Pages/Course/EditCourse.jsx
+++ Client/src/Pages/Course/EditCourse.jsx
@@ -142,7 +142,7 @@ const EditCourse = () => {
             const course = thunkResponse?.payload?.Data?.updatedCourse
 
             if (course) {
-                navigate(`/courseDetail`, { state: { course } });
+                navigate(`/course/detail/${course?._id}`);
             }
         } else {
             return
